21b95a05118ec6509ce70b2fb43cd82c9db5cd2e
Aman Bhardwaj <dev-backup@proqure.io>
2023-05-25 15:55:18 +0530

    Initial Commited truffle

---
+++ truffle-config.js
@@ -0,0 +1,57 @@
+/**
+ *
+ * https://trufflesuite.com/docs/truffle/reference/configuration
+ 
+ * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
+ */
+
+// require('dotenv').config();
+// const { MNEMONIC, PROJECT_ID } = process.env;
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+
+  contracts_build_directory: "./funding/public/contracts",
+
+  networks: {
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.19",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // db: {
+  //   enabled: false,
+  //   host: "127.0.0.1",
+  //   adapter: {
+  //     name: "indexeddb",
+  //     settings: {
+  //       directory: ".db"
+  //     }
+  //   }
+  // }
+};
